657bc6e375f5b8c254759485d3e192568275fe02
thirdhousedeveloper <enockson11@gmail.com>
2023-05-09 21:08:12 +0100

    stamp on receipt

--- printreceipt.php
+++ printreceipt.php
@@ -14,17 +14,24 @@ $matNum=$_SESSION['matNum'];
 ?>
 
 <div class="container">
-<div class="jumbotron">
+  <div class="jumbotron">
     <div class="alert alert-info"><h1>STUDENT RECEIPT</h1></div>
     <h3>NAME: <?php echo $row["stName"]; ?></h3>
     <h2>Registration number: <?php echo $row["matNum"]; ?></h2>
     <h2>LEVEL: <?php echo $row["level"]; ?>L</h2>
     <div class="alert alert-success">
-        <h2>Total Paid: <?php echo $row["transactionAmount"]; ?></h2>
+      <h2>Total Paid: <?php echo $row["transactionAmount"]; ?></h2>
     </div>
-    <h3>Bursar Sign:.........................................</h3>
-
+    <div class="row">
+      <div class="col-md-6">
+          <img src="img/receipt.JPEG" alt="Bursar Sign" style="max-width:200%; width:150px; margin-left: 100px; height:auto; border-radius:50%;">
+          
+          <h3>Bursery Sign:.....................</h3>
+      </div>
+    </div>
+  </div>
 </div>
+
 
 <?php 
  }
